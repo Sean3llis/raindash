61196477f784607a82930c4c0855b2405e9934d6
Sean Ellis <seane235@gmail.com>
2016-11-06 10:17:34 -0500

    remove key from source

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 'use strict';
 import axios from 'axios';
 
-const API_KEY = 'c7100b6f2600d083f8839495e834fe50';
+import API_KEY from '../../private.js';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=imperial`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 export const REQUESTING_WEATHER = 'REQUESTING_WEATHER';
