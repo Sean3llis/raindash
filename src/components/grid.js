45c9394b6374489e12cbbde1903511faa20439db
Sean Ellis <seane235@gmail.com>
2016-11-22 08:52:53 -0500

    style tweaks

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export default props => {
-  const squareUnit = 12;
+  const squareUnit = 6;
   return (
     <svg className="grid" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="400px" height="300px">
       <defs>
         <pattern id="grid-pattern" width={squareUnit} height={squareUnit} patternUnits="userSpaceOnUse">
-          <path d="M 0 0 L 12 0 12 12 0 12 z" stroke="#f5f5f5" strokeWidth="1" fill="none"></path>
+          <path d="M 0 0 L 12 0 12 12 0 12 z" stroke="#ecf0f1" strokeWidth="1" fill="none"></path>
         </pattern>
       </defs>
       <rect fill="url(#grid-pattern)" height="100%" width="100%" y="2" x="10" />
